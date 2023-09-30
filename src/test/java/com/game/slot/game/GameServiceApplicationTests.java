f63eccdadf3d6c1c51b0437e5206e5efd98eef9e
Vitaliy <vitalik.voronov@gmail.com>
2023-09-30 12:21:29 +0200

    first run

---
+++ src/test/java/com/game/slot/game/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.slot.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
